fe3faefe0b63b5b7fce791698b5aa18d4817d0a7
StivenKatuuk <stiven.katuuk@simajji.com>
2024-01-29 09:02:55 +0800

    refactor(action): app to command to better naming

--- src/actions/ResolveMessageAction.ts
+++ src/actions/ResolveMessageAction.ts
@@ -1,7 +1,7 @@
 import { WASocket } from '@whiskeysockets/baileys';
 import { BaileysEventMap } from '@whiskeysockets/baileys/lib/Types';
 import { WAMessage} from '@whiskeysockets/baileys/lib/Types/Message';
-import app from "../configs/command";
+import command from "../configs/command";
 import {patternsAndTextIsMatch} from "../supports/Str";
 
 class ResolveMessageAction {
@@ -9,7 +9,7 @@ class ResolveMessageAction {
     const messageInformations: WAMessage[] = messages.messages
 
     for(const message of messageInformations) {
-      for(const handler of app.messageHandlers) {
+      for(const handler of command.messageHandlers) {
         if(patternsAndTextIsMatch(handler.patterns, message)) {
           await handler.concrete.execute(message, socket)
 
