a0a2a41cd63b9b966841c8922a47459bb351f41f
Stiven Trizky Katuuk <pemudakoding@gmail.com>
2024-02-13 13:34:37 +0800

    build(core): process management

---
+++ ecosystem.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+        {
+            name: 'AJG BOT',
+            script: 'dist/index.js',
+            instances: '1',
+            exec_mode: 'fork',
+        }
+    ]
+};
