834872b91c61c3ebd46b4369a5843f2f487c0a51
Stiven Trizky Katuuk <pemudakoding@gmail.com>
2024-03-11 20:51:28 +0800

    fix(actions): not strictly finding match bardword

--- src/actions/database/CheckIsTextContainBadwordsAction.ts
+++ src/actions/database/CheckIsTextContainBadwordsAction.ts
@@ -4,8 +4,9 @@ export default class CheckIsTextContainBadwordsAction {
     public static async execute(text: string): Promise<boolean> {
        try {
            const words = await badwordDatabase.getData('/words')
+           const pattern = new RegExp("\\b(" + words.join("|") + ")\\b", "i");
 
-           return words.some((word: string) => text.toLowerCase().includes(word))
+           return pattern.test(text);
        } catch (Error) {
            console.log('words is not available yet')
 
