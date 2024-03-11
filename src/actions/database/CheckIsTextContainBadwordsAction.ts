8bdb3862bb05131e1164be7196ca15e75e9f5339
Stiven Trizky Katuuk <pemudakoding@gmail.com>
2024-03-11 22:05:26 +0800

    Revert "fix: badword matcher able to identify the end of the string"
    This reverts commit 53d6ba2e079f4d57720fdea4a541f8cb8fdc7c8d.

--- src/actions/database/CheckIsTextContainBadwordsAction.ts
+++ src/actions/database/CheckIsTextContainBadwordsAction.ts
@@ -4,16 +4,9 @@ export default class CheckIsTextContainBadwordsAction {
     public static async execute(text: string): Promise<boolean> {
        try {
            const words = await badwordDatabase.getData('/words')
+           const pattern = new RegExp("\\b(" + words.join("|") + ")\\b", "i");
 
-           const inputLowercase = text.toLowerCase(); // Convert input to lowercase for case-insensitive matching
-           for (let i = 0; i < words.length; i++) {
-               const badWord = words[i];
-               if (inputLowercase.indexOf(badWord) !== -1) {
-                   return true;
-               }
-           }
-
-           return false;
+           return pattern.test(text);
        } catch (Error) {
            console.log('words is not available yet')
 
