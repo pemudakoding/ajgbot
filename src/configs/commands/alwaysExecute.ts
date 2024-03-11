2e53cdd7b3d871a9d1d728bd0c1f39a5798ba8a8
Stiven Trizky Katuuk <pemudakoding@gmail.com>
2024-03-11 16:23:59 +0800

    build(config): activating anti secret comman

--- src/configs/commands/alwaysExecute.ts
+++ src/configs/commands/alwaysExecute.ts
@@ -2,13 +2,13 @@ import BaseMessageHandlerAction from "../../foundation/actions/BaseMessageHandle
 import MessageHandler from "../../types/MessageHandler";
 import {resolveCommandHandlerConfig} from "../../supports/Command";
 import AnswerThanksAction from "../../actions/message/alwaysExecuted/AnswerThanksAction";
-// import ResolveAntiSecretAction from "../../actions/message/alwaysExecuted/ResolveAntiSecretAction";
+import ResolveAntiSecretAction from "../../actions/message/alwaysExecuted/ResolveAntiSecretAction";
 import SaveMessageAction from "../../actions/message/alwaysExecuted/SaveMessageAction";
 
 const handlers: BaseMessageHandlerAction[] = [
     new AnswerThanksAction(),
     new SaveMessageAction(),
-    // new ResolveAntiSecretAction(),
+    new ResolveAntiSecretAction(),
 ];
 
 export default <MessageHandler[]> resolveCommandHandlerConfig(handlers)
